4c69d9092b31666bec19fb3c91886366e35e1f44
ayushvyas17 <aykvyas@gmail.com>
2024-08-04 23:56:45 +0530

    sec

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api/v1':'https://your-own-todo.onrender.com',
+      '/api/v1':'https://yourowntodo.onrender.com',
     },
   },
   plugins: [react()],
